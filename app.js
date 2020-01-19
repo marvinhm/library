3e3f49f5d88dd7a5cf52951ce638f8c850a4b7a8
Marvin Matovu <marvinmatovu@gmail.com>
2020-01-19 22:57:28 +0000

    serving up index

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ let express = require('express');
 let chalk = require('chalk');
 let debug = require('debug')('app');
 let morgan = require('morgan');
+let path = require('path');
 
 // stuff doing outside of requires
 let app = express();
 
 app.use(morgan('tiny'));
 app.get('/', (req, res) => {
-  res.send("Hello form my library app");
+  res.sendFile(path.join(__dirname, "views",  "/index.html"));
 });
 
 app.listen(5000, () => {
